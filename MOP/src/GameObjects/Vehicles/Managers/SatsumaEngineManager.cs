43fd675d5f3ee7c8d8b3705a079fbcd8598fab8c
Athlon <aathlon@outlook.com>
2020-04-21 10:30:23 +0200

    Satsuma Engine Manager

---
+++ MOP/src/GameObjects/Vehicles/Managers/SatsumaEngineManager.cs
@@ -0,0 +1,36 @@
+﻿// Modern Optimization Plugin
+// Copyright(C) 2019-2020 Athlon
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using UnityEngine;
+
+namespace MOP
+{
+    class SatsumaEngineManager : MonoBehaviour
+    {
+        bool afterGameEnabled;
+
+        void OnEnable()
+        {
+            if (!afterGameEnabled)
+            {
+                afterGameEnabled = true;
+                return;
+            }
+
+            Object.Destroy(GetComponent<HingeJoint>());
+        }
+    }
+}
