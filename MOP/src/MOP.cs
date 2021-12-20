d9004c4a689b725e1e3eec6712ff566b3362396c
Athlon007 <aathlon@outlook.com>
2021-12-20 17:50:56 +0100

    Update MOP.cs

--- MOP/src/MOP.cs
+++ MOP/src/MOP.cs
@@ -37,7 +37,7 @@ namespace MOP
         public override string Name => "MODERN OPTIMIZATION PLUGIN";
 #endif
         public override string Author => "Athlon"; //Your Username
-        public override string Version => "3.3.8"; //Version
+        public override string Version => "3.3.9"; //Version
         public const string SubVersion = ""; // NIGHTLY-yyyymmdd | BETA_x | RC_
         public override string UpdateLink => "https://github.com/Athlon007/MOP";
         public override byte[] Icon => Properties.Resources.icon;
