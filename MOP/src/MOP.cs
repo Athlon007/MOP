1e6dcab7a695494215e30299841bf319b02321e2
Athlon007 <aathlon@outlook.com>
2022-02-13 15:06:35 +0100

    3.4.3 Beta 1

--- MOP/src/MOP.cs
+++ MOP/src/MOP.cs
@@ -31,8 +31,8 @@ namespace MOP
         public override string ID => "MOP";
         public override string Name => "MODERN OPTIMIZATION PLUGIN";
         public override string Author => "Athlon"; //Your Username
-        public override string Version => "3.4.2"; //Version
-        public const string SubVersion = ""; // NIGHTLY-yyyymmdd | BETA_x | RC_
+        public override string Version => "3.4.3"; //Version
+        public const string SubVersion = "BETA_1"; // NIGHTLY-yyyymmdd | BETA_x | RC_
         public override string Description => "The ultimate My Summer Car optimization project!";
         public override byte[] Icon => Properties.Resources.icon;
 
