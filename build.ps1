e728ef848a7fe0833e8e3906a88e34524821d935
Athlon007 <aathlon@outlook.com>
2022-02-25 15:52:11 +0100

    Updated the build file

---
+++ build.ps1
@@ -0,0 +1,4 @@
+Write-Host "Packing MOP..."
+Compress-Archive -Path "MOP\bin\Release\MOP.dll" -DestinationPath "build\MOP.zip" -Force
+Compress-Archive -Path "MOP\bin\ProRelease\MOP.dll" -DestinationPath "build\MOP.pro.zip" -Force
+Write-Host "Done!"
