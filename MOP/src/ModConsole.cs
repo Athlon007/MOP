ff6ddda213f2177912e91969a340241f256311b5
Athlon007 <aathlon@outlook.com>
2022-04-08 00:31:00 +0200

    Console capture optimization

--- MOP/src/ModConsole.cs
+++ MOP/src/ModConsole.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MOP
 {
@@ -47,13 +48,26 @@ namespace MOP
         static void Add(string message)
         {
             message = message.Replace("[MOP] ", "");
-            message = message.Replace("</color>", "");
+            //message = message.Replace("</color>", "");
             message = message.Replace("<color=yellow>", "[WARNING] ");
             message = message.Replace("<color=green>", "[SYSTEM] ");
             message = message.Replace("<color=red>", "[ERROR] ");
+            //message = message.Replace("<b>", "");
+            //message = message.Replace("</b>", "");
             message = message.Trim();
             message = DateTime.Now.ToString("HH:mm:ss") + ": " + message;
-            messages.Add(message);
+
+            string[] messageArray = message.Split('\n');
+            for (int i = 0; i < messageArray.Length; ++i)
+            {
+                if (i > 0)
+                {
+                    messageArray[i] = new string(' ', 10) + messageArray[i];
+                }
+                messageArray[i] = Regex.Replace(messageArray[i], "<(.*?)>", "");
+            }
+
+            messages.Add(string.Join("\n", messageArray));
         }
 
         public static List<string> GetMessages()
