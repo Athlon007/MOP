2e296aa6b888caef771b67fbd6ebdcccf1567861
Athlon007 <aathlon@outlook.com>
2022-08-30 12:14:12 +0200

    Update build.ps1

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -10,3 +10,5 @@ Write-Host "`n`nBuilding done! Packing MOP..."
 Compress-Archive -Path "..\MOP\bin\Release\MOP.dll" -DestinationPath "..\build\MOP.zip" -Force
 Compress-Archive -Path "..\MOP\bin\ProRelease\MOP.dll" -DestinationPath "..\build\MOP.pro.zip" -Force
 Write-Host "Done!"
+
+Invoke-Item "..\build"
